4f90cf14d86a32e090f002961aef6648abf39357
MatzeH157 <MatzeH157@f2f8bf26-27bb-74d3-be08-1e18597623ec>
2010-05-04 16:29:37 +0000

    Hausaufgaben von Blatt 4 wie ich sie im bscw hochgeladen hab

---
+++ trunk/AlgoDat/SoSe10/src/algodat/ha/blatt04/BNode.java
@@ -0,0 +1,89 @@
+
+public class BNode {
+
+	private BNode parentRef;
+	private int anzElemente;
+	private int[] myWerte;
+	private BNode[] myRefs;
+	private int ordnung;
+	
+	public BNode(BNode parent, int wert, int ordnung) {		// ordnung muss der des baumes entsprechen!
+		this.ordnung = ordnung;
+		myWerte = new int[(2*this.ordnung)+1]; // +1 für Überfüllung
+		this.parentRef = parent;
+		this.anzElemente = 0;
+		myRefs = new BNode[((2*this.ordnung)+1)];
+		for(int i = 0; i < 2*this.ordnung+1; ++i)
+			myRefs[i] = null;
+		add(wert);
+	}
+	
+	public void add(int wert) {
+		this.myWerte[this.anzElemente] = wert;
+		this.anzElemente++;
+		sort();
+	}
+	
+	public void sort() {
+		int tmp;
+		for(int i = 0; i < anzElemente-1; ++i) {
+			for(int j = i; j >= 0; --j) {
+				if(myWerte[j+1] < myWerte[j]) {
+					tmp = myWerte[j];
+					myWerte[j] = myWerte[j+1];
+					myWerte[j+1] = tmp;
+				}
+			}
+		}
+	}
+	
+	public BNode getRef(int index) {
+		return (0 <= index && 2*this.ordnung+1 > index ? myRefs[index] : null);
+	}
+	
+	public void setRef(int idx, BNode ref) {
+		myRefs[idx] = ref;
+	}
+	
+	public void remove(int idx) {
+		for(int i = idx; i < this.anzElemente-1; ++i) {
+			this.myWerte[i] = this.myWerte[i+1];
+		}
+		--this.anzElemente;
+	}
+	
+	public int[] getWerte() {
+		return this.myWerte;
+	}
+	
+	public boolean isFull() {
+		return (anzElemente > 2 * this.ordnung);
+	}
+	
+	public boolean isUnderflow() {
+		return (this.anzElemente < this.ordnung);
+	}
+	
+	public BNode getParent() {
+		return this.parentRef;
+	}
+	
+	public int getCount() {
+		return this.anzElemente;
+	}
+	
+	public int getOrd() {
+		return this.ordnung;
+	}
+	
+	public boolean isBlatt() {
+		boolean ret = false;
+		for(int i = 0; i < 2*this.ordnung + 1; ++i) {
+			if(myRefs[i] != null) {
+				ret = true;
+				break;
+			}
+		}
+		return ret;
+	}
+}
