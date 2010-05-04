ed6ce8505db2ee0527bde85e80bdbdcd393e0e4a
MatzeH157 <MatzeH157@f2f8bf26-27bb-74d3-be08-1e18597623ec>
2010-05-04 16:29:37 +0000

    Hausaufgaben von Blatt 4 wie ich sie im bscw hochgeladen hab

--- AlgoDat/SoSe10/src/algodat/ha/blatt04/BTree.java
+++ AlgoDat/SoSe10/src/algodat/ha/blatt04/BTree.java
@@ -1,74 +1,80 @@
-package algodat.ha.blatt04;
-
-import java.util.ArrayList;
-
 public class BTree {
-	private Node root;
-	
-	class Node{
-		/**ArrayList enthält Werte des Knotens*/
-		private ArrayList<Integer> values = new ArrayList<Integer>();
-		/**ArrayList enthält Referenzen des Knotens*/
-		private ArrayList<Node> references = new ArrayList<Node>();
-		
-		public int getValue(int i){
-			return values.get(i);
-		}
-		
-		public void addValue(int i, int pos){
-			if (pos == -1){
-				//am ende einfügen
-				values.add(i);
-			}else {
-				values.add(pos, i);
-			}
-		}
-	
-		public Node getReference(int i){
-			return references.get(i);
-		}
-		
-		public void addReference(Node i, int pos){
-			if (pos == -1){
-				//am ende einfügen
-				references.add(i);
-			}else {
-				references.add(pos, i);
-			}
-		}
-		
-		public boolean hasReferences(){
-			return references.isEmpty();
-		}
-		
-		public int valueCount(){
-			return values.size();
-		}
-		
-		public int referenceCount(){
-			return references.size();
-		}
-		
+
+	private BNode wurzel;
+	private BNode searchBlatt;
+	private int ordnung;
+
+	public BTree(BNode wurzel, int ord) {
+		this.wurzel = wurzel;
+		this.searchBlatt = null;
+		this.ordnung = ord;
 	}
-	
-	public Node search (int value){
-		Node node = this.root;
-		while (node.hasReferences()){
-			for(int i = 0; i < node.valueCount(); i++){
-				if (i < node.getValue(i)){
-					node = node.getReference(i);
-					break;
-				} else if(i == node.valueCount() - 1){
-					node = node.getReference(++i);
+
+	public void insert(int wert) {
+		search(wert);
+		if(this.searchBlatt != null) {
+			insert(this.searchBlatt, wert);
+		} else {
+			insert(this.wurzel, wert);
+		}
+	}
+
+	private void insert(BNode node, int wert) {
+		node.add(wert);
+		if (node.isFull()) {
+			int tmp = node.getWerte()[this.ordnung];
+			BNode current = new BNode(node.getParent(), tmp, this.ordnung);
+			for (int i = 2 * this.ordnung; i >= this.ordnung; --i) {
+				current.add(node.getWerte()[i]);
+				node.remove(i);
+			}
+			if (node == this.wurzel) {
+				BNode parent = new BNode(null, tmp, this.ordnung);
+				parent.setRef(0, node);
+				//node.remove(this.ordnung);
+				parent.setRef(1, current);
+				this.wurzel = parent;
+				if (parent.isFull()) {
+					insert(parent, wert);
 				}
 			}
 		}
-		
-		return node;
 	}
-	
-	public void insert (int value){
-		
+
+	public boolean search(int search) {
+		return search(search, wurzel);
 	}
-}	
-  
+
+	private boolean search(int search, BNode iter) {
+
+		boolean ret = false;
+		if (iter != null) {
+			for (int i = 0; i < iter.getCount(); ++i) {
+				if (search == iter.getWerte()[i]) {
+					ret = true;
+					break;
+				}
+			}
+
+			this.searchBlatt = iter;
+			if (!ret) {
+				boolean gefunden = false;
+				for (int i = 0; i < iter.getCount(); ++i) {
+					if (search > iter.getWerte()[i]) {
+						gefunden = true;
+						search(search, iter.getRef(i));
+					}
+				}
+				if (!gefunden) {
+					search(search, iter.getRef(2 * iter.getOrd() + 1));
+				}
+			}
+		}
+
+		return ret;
+	}
+
+	public BNode getWurzel() {
+		return this.wurzel;
+	}
+}
---
+++ AlgoDat/SoSe10/src/algodat/ha/blatt04/TestBTree.java
@@ -0,0 +1,16 @@
+
+public class TestBTree {
+
+	public static void main(String args[]) {
+	
+		int ord = 2;
+		BNode wurz = new BNode(null, 20, ord);
+		BTree baum = new BTree(wurz, ord);
+		baum.insert(25);
+		baum.insert(15);
+		baum.insert(5);
+		baum.insert(35);
+		
+	}
+	
+}
